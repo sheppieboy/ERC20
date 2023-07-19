0afbc8d094995f10e508343265f6745906c32ebe
Luke Sheppard <sheppiegit@gmail.com>
2023-07-19 18:11:40 -0400

    added appropriate naming conventions and fixed compiler difference between IERC20.sol and ERC20.sol.

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
-    uint256 private totalSupply;
-    mapping(address owner => uint256 amount) private balances;
-    mapping(address owner => mapping(address spender => uint256 amount)) private allowance;
-    string private name;
-    string private symbol;
-    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals = 18;
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-        balanceOf[msg.sender] += _totalSupply;
-        totalSupply = _totalSupply;
-        _mint(msg.sender, _totalSupply);
+    mapping(address owner => uint256 amount) private _balances;
+    mapping(address owner => mapping(address spender => uint256 amount)) private _allowance;
+
+    constructor(string memory name_, string memory symbol_, uint256 totalSupply_) {
+        _name = name_;
+        _symbol = symbol_;
+        balanceOf[msg.sender] += totalSupply_;
+        _totalSupply = totalSupply_;
+        _mint(msg.sender, totalSupply_);
     }
 
     //external functions
@@ -28,26 +31,26 @@ contract ERC20 is IERC20 {
 
     //public view functions
     function name() public view virtual returns (string memory) {
-        return name;
+        return _name;
     }
 
     function symbol() public view virtual returns (string memory) {
-        return symbol;
+        return _symbol;
     }
 
     function totalSupply() public view virtual returns (uint256) {
-        return totalSupply;
+        return _totalSupply;
     }
 
     function decimals() public view virtual returns (uint256) {
-        return decimals;
+        return _decimals;
     }
 
     function balanceOf(address account) public view virtual returns (uint256) {
-        return balances[account];
+        return _balances[account];
     }
 
     function allowance(address owner, address spender) public view virtual returns (uint256) {
-        return allowance[owner][spender];
+        return _allowance[owner][spender];
     }
 }
